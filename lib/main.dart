19e664e9211e24f5101adc282e327041b562759e
Ahmed Khaled <ahmedk@example.com>
2025-04-12 09:43:35 +0100

    first

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
+import 'package:untitled2/generated/l10n.dart';
+import 'package:untitled2/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: const Locale('en'),
+      localizationsDelegates: [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primaryColorLight: Color.fromARGB(255, 0, 122, 255),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+bool isArabic(){
+  return Intl.getCurrentLocale() == 'ar';
+
+}
